16a208a10e860b2a90c6fe30a594531a957e6cb8
Anant Singh <anantsingh@aristocrat.com>
2024-05-07 14:46:06 +0530

    Frontend init

--- index.js
+++ index.js
@@ -2,32 +2,28 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
 const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
 });
 
 app.use(cors());
-
-app.get("/", (req, res, next) => {
-  res.status(200).send("Server Response");
+const PORT = process.env.PORT || 8090;
+app.get('/', (req, res) => {
+        res.send('Hello World');
 });
 
 io.on("connection", (socket) => {
-  socket.emit("me", socket.id);
-  console.log(socket.id);
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-
-  socket.on("message-user", (toUser) => {
-    io.to(toUser.socketId).emit("message", toUser.payload);
-  });
+    socket.emit("me", socket.id);
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded")
+    });
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+    });
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal)
+    });
 });
-
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
